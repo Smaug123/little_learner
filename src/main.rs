27be82c14a20428eec00a6558786ffddb5f5023f
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-03-19 17:39:35 +0000

    Add extension operation

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,53 @@ macro_rules! tensor {
     ($x:ty , $i: expr, $($is:expr),+) => {[tensor!($x, $($is),+); $i]};
 }
 
+pub trait Extensible<A> {
+    fn apply<F>(&self, other: &Self, op: &F) -> Self
+    where
+        F: Fn(&A, &A) -> A;
+}
+
+impl<A, T, const N: usize> Extensible<A> for [T; N]
+where
+    T: Extensible<A> + Copy + Default,
+{
+    fn apply<F>(&self, other: &Self, op: &F) -> Self
+    where
+        F: Fn(&A, &A) -> A,
+    {
+        let mut result = [Default::default(); N];
+        for (i, coord) in self.iter().enumerate() {
+            result[i] = T::apply(coord, &other[i], op);
+        }
+        result
+    }
+}
+
+#[macro_export]
+macro_rules! extensible {
+    ($x: ty) => {
+        impl Extensible<$x> for $x {
+            fn apply<F>(&self, other: &Self, op: &F) -> Self
+            where
+                F: Fn(&Self, &Self) -> Self,
+            {
+                op(self, other)
+            }
+        }
+    };
+}
+
+extensible!(u8);
+extensible!(f64);
+
+pub fn extension<T, A, F>(t1: &T, t2: &T, op: F) -> T
+where
+    T: Extensible<A>,
+    F: Fn(&A, &A) -> A,
+{
+    t1.apply::<F>(t2, &op)
+}
+
 fn main() {
     let y = line(&[1.0, 7.3], &linear_params_2d(3.0, 1.0));
     println!("{:?}", y);
@@ -54,7 +101,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_tensor() {
+    fn test_tensor_type() {
         let _: tensor!(f64, 1, 2, 3) = [[[1.0, 3.0, 6.0], [-1.3, -30.0, -0.0]]];
+    }
+
+    #[test]
+    fn test_extension() {
+        let x: tensor!(u8, 1) = [2];
+        let y: tensor!(u8, 1) = [7];
+        assert_eq!(extension(&x, &y, |x, y| x + y), [9]);
+
+        let x: tensor!(u8, 3) = [5, 6, 7];
+        let y: tensor!(u8, 3) = [2, 0, 1];
+        assert_eq!(extension(&x, &y, |x, y| x + y), [7, 6, 8]);
+
+        let x: tensor!(u8, 2, 3) = [[4, 6, 7], [2, 0, 1]];
+        let y: tensor!(u8, 2, 3) = [[1, 2, 2], [6, 3, 1]];
+        assert_eq!(extension(&x, &y, |x, y| x + y), [[5, 8, 9], [8, 3, 2]]);
     }
 }
